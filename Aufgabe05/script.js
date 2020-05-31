05dcc9b8df6582dc9cccd3465da02470e4b42a2a
Christian <mr.chris.chris1@gmail.com>
2020-05-31 17:23:53 +0200

    y

---
+++ Aufgabe05/script.js
@@ -0,0 +1,108 @@
+"use strict";
+(function (Wasser) {
+    let Mineralwasser1 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser1",
+    };
+    let Mineralwasser2 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser2",
+    };
+    let Mineralwasser3 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser3",
+    };
+    let Mineralwasser4 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser4",
+    };
+    let Mineralwasser5 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser5",
+    };
+    let Mineralwasser6 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser6",
+    };
+    let Mineralwasser7 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser7",
+    };
+    let Mineralwasser8 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser8",
+    };
+    let Mineralwasser9 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser9",
+    };
+    let Mineralwasser10 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser10",
+    };
+    let Mineralwasser11 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser11",
+    };
+    let Mineralwasser12 = {
+        _img: "35193-1.png",
+        _beschreibung: "superwasser12",
+    };
+    let wasserProducts = [Mineralwasser1, Mineralwasser2, Mineralwasser3, Mineralwasser4, Mineralwasser5, Mineralwasser6, Mineralwasser7, Mineralwasser8, Mineralwasser9, Mineralwasser10, Mineralwasser12];
+    for (let _index = 0; _index < wasserProducts.length; _index++) {
+    }
+    (function (Softdrinks) {
+        let Softdrink1 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink1",
+        };
+        let Softdrink2 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink2",
+        };
+        let Softdrink3 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink3",
+        };
+        let Softdrink4 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink4",
+        };
+        let Softdrink5 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink5",
+        };
+        let Softdrink6 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink6",
+        };
+        let Softdrink7 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink7",
+        };
+        let Softdrink8 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink8",
+        };
+        let Softdrink9 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink9",
+        };
+        let Softdrink10 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink10",
+        };
+        let Softdrink11 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink11",
+        };
+        let Softdrink12 = {
+            _img: "35193-1.png",
+            _beschreibung: "Softdrink12",
+        };
+    });
+    let softProducts = [Mineralwasser1, Mineralwasser2, Mineralwasser3, Mineralwasser4, Mineralwasser5, Mineralwasser6, Mineralwasser7, Mineralwasser8, Mineralwasser9, Mineralwasser10, Mineralwasser12];
+    for (let _index = 0; _index < softProducts.length; _index++) {
+    }
+});
+//# sourceMappingURL=script.js.map
