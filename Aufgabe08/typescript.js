64cf42f480c358762a1cd37f1ac7a190433e7905
Christian <mr.chris.chris1@gmail.com>
2020-06-21 15:17:16 +0200

    aufgabe8

--- Aufgabe08/typescript.js
+++ Aufgabe08/typescript.js
@@ -1,2 +1,17 @@
 "use strict";
+var Aufgabe08;
+(function (Aufgabe08) {
+    let formData;
+    let abschicken = document.getElementById("abschicken");
+    abschicken.addEventListener("click", abschickenklick);
+    async function abschickenklick() {
+        formData = new FormData(document.forms[0]);
+        let url = "https://gissommersemester2020.herokuapp.com/";
+        let urlquery = new URLSearchParams(formData);
+        url = url + "?" + urlquery.toString();
+        let antwort = await fetch(url);
+        let antworttext = await antwort.text();
+        console.log(antworttext);
+    }
+})(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=typescript.js.map
--- Aufgabe08/typescript.ts
+++ Aufgabe08/typescript.ts
@@ -1,3 +1,16 @@
 namespace Aufgabe08 {
+    let formData: FormData;
+    let abschicken: HTMLElement = <HTMLButtonElement>document.getElementById("abschicken");
+    abschicken.addEventListener("click", abschickenklick);
 
+    async function abschickenklick(): Promise <void> {
+        formData = new FormData(document.forms[0]);
+        let url: string = "https://gissommersemester2020.herokuapp.com/";
+        let urlquery: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + urlquery.toString();
+
+        let antwort: Response = await fetch(url);
+        let antworttext: string = await antwort.text();
+        console.log(antworttext);
+    }
 }
