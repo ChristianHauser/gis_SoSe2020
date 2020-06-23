d694b922c183c5e5ef8c5fa20ae8eacb7fdf3b90
Christian <mr.chris.chris1@gmail.com>
2020-06-23 20:56:19 +0200

    x

--- Aufgabe08/typescript.ts
+++ Aufgabe08/typescript.ts
@@ -8,6 +8,7 @@ function handleClick(): void {
 
         let formData: FormData = new FormData(document.forms[0]);
         let url: string = "https://gissose2020chris.herokuapp.com";
+        
         let query: URLSearchParams = new URLSearchParams(<any>formData);
         url = url + "?" + query.toString();
 
