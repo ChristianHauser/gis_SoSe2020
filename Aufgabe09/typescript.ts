b8b416a863463e76ab97c548bea1a38af9259896
Christian <mr.chris.chris1@gmail.com>
2020-06-28 15:21:08 +0200

    xx

--- Aufgabe09/typescript.ts
+++ Aufgabe09/typescript.ts
@@ -1,46 +1,38 @@
-namespace namespace09 {
+namespace Aufgabe9 {
+    let buttonhtml: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button1");
+    buttonhtml.addEventListener("click", buttonclickhtml);
 
-    document.getElementById("button1")?.addEventListener("click", htmlausgabebutton);
-    document.getElementById("button2")?.addEventListener("click", consolenausgabebutton);
+    let buttonjson: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button2");
+    buttonjson.addEventListener("click", buttonclickjson);
 
-    function senden(): string {
 
+    async function buttonclickhtml(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
+        //let url: string = "http://localhost:8100";
         let url: string = "https://gissose2020chris.herokuapp.com";
-        //let url: string = "http://localhost:8100/";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url + "?" + query.toString();
-        return url;
-    }
-    // ansteuerung des 1 Buttons 
-    async function consolenausgabebutton(): Promise<void> {
-        consolenausgabe(await splitrequest(senden()));
-    }
-    // ansteuerung des 2 Buttons 
-    async function htmlausgabebutton(): Promise<void> {
-        htmlausgabe( await splitrequest(senden()));
-    }
-
-    // ausgabe des Strngs als html 
-    function htmlausgabe(_arraysplit: string[]): void {
-
-        (<HTMLElement>document.getElementById("anzeige")).innerHTML  = _arraysplit[0];
+        url = url + "/html" + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let responseString: string = await response.text();
+        let serverResponse: HTMLElement = <HTMLElement> document.getElementById("anzeige");        
+        serverResponse.innerHTML = responseString;
 
     }
-    // ausgebe der json in der Console
-    function consolenausgabe(_arraysplit: string[]): void {
-        let ausgabe: string = JSON.parse(_arraysplit[1]);
-        console.log(ausgabe);
+
+
+    async function buttonclickjson(): Promise<void> {
+        let formData: FormData = new FormData(document.forms[0]);
+        
+        //let url: string = "http://localhost:8100";
+        let url: string = "https://gissose2020chris.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "/json" + "?" + query.toString();
+        await fetch(url);
+        for (let entry of query) {
+            console.log(entry);
+            console.log("Objekt: " + entry[0] + " Wert:" + entry[1]);
+        }
+      
     }
 
-    // die response an der verhervestgelekten teil trennen 
-    async function splitrequest(_url: RequestInfo): Promise<string[]> {
-        let response: Response = await fetch(_url);
-        let response2: string = await response.text();
-        let arrayrequest: string[] = response2.split("&&");
-        return arrayrequest;
-    }
-
-
-
-}   
+}
