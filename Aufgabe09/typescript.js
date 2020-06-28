be46c8ae32f3072f5ade4ecc19a0c94267043db2
Christian <mr.chris.chris1@gmail.com>
2020-06-28 12:33:51 +0200

    x

--- Aufgabe09/typescript.js
+++ Aufgabe09/typescript.js
@@ -1,34 +1,39 @@
 "use strict";
-var Aufgabe09;
-(function (Aufgabe09) {
-    let buttonjson = document.getElementById("button1");
-    buttonjson.addEventListener("click", handlejson);
-    let buttonhtml = document.getElementById("button2");
-    buttonhtml.addEventListener("click", handlehtml);
-    let server = document.getElementById("server");
-    async function handlehtml() {
+var namespace09;
+(function (namespace09) {
+    document.getElementById("button1")?.addEventListener("click", htmlausgabebutton);
+    document.getElementById("button2")?.addEventListener("click", consolenausgabebutton);
+    function senden() {
         let formData = new FormData(document.forms[0]);
         let url = "https://gissose2020chris.herokuapp.com";
-        url += "/html";
+        //let url: string = "http://localhost:8100/";
         let query = new URLSearchParams(formData);
-        url = url += "?" + query.toString();
-        let response = await fetch(url);
-        let responseText = await response.text();
-        let server = document.getElementById("server");
-        server.innerHTML = responseText;
+        url = url + "?" + query.toString();
+        return url;
     }
-    async function handlejson() {
-        let formData = new FormData(document.forms[0]);
-        let url = "https://gissose2020chris.herokuapp.com";
-        url += "/json";
-        let query = new URLSearchParams(formData);
-        url = url += "?" + query.toString();
-        let response = await fetch(url);
-        let responseText = await response.text();
-        let responseJSON = JSON.parse(responseText);
-        console.log(responseJSON);
-        console.log(responseText);
-        server.innerHTML = responseText;
+    // ansteuerung des 1 Buttons 
+    async function consolenausgabebutton() {
+        consolenausgabe(await splitrequest(senden()));
     }
-})(Aufgabe09 || (Aufgabe09 = {}));
+    // ansteuerung des 2 Buttons 
+    async function htmlausgabebutton() {
+        htmlausgabe(await splitrequest(senden()));
+    }
+    // ausgabe des Strngs als html 
+    function htmlausgabe(_arraysplit) {
+        document.getElementById("anzeige").innerHTML = _arraysplit[0];
+    }
+    // ausgebe der json in der Console
+    function consolenausgabe(_arraysplit) {
+        let ausgabe = JSON.parse(_arraysplit[1]);
+        console.log(ausgabe);
+    }
+    // die response an der verhervestgelekten teil trennen 
+    async function splitrequest(_url) {
+        let response = await fetch(_url);
+        let response2 = await response.text();
+        let arrayrequest = response2.split("&&");
+        return arrayrequest;
+    }
+})(namespace09 || (namespace09 = {}));
 //# sourceMappingURL=typescript.js.map
--- Aufgabe09/typescript.ts
+++ Aufgabe09/typescript.ts
@@ -1,43 +1,46 @@
-namespace Aufgabe09 {
+namespace namespace09 {
 
-    let buttonjson: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button1");
-    buttonjson.addEventListener("click", handlejson);
-    let buttonhtml: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button2");
-    buttonhtml.addEventListener("click", handlehtml);
+    document.getElementById("button1")?.addEventListener("click", htmlausgabebutton);
+    document.getElementById("button2")?.addEventListener("click", consolenausgabebutton);
 
-    let server: HTMLElement = <HTMLElement>document.getElementById("server");
+    function senden(): string {
 
-
-    async function handlehtml(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
         let url: string = "https://gissose2020chris.herokuapp.com";
-        url += "/html";
+        //let url: string = "http://localhost:8100/";
         let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url += "?" + query.toString();
-        let response: Response = await fetch(url);
-        let responseText: string = await response.text();
-        let server: HTMLElement = <HTMLElement>document.getElementById("server");
-        server.innerHTML = responseText;
-
+        url = url + "?" + query.toString();
+        return url;
     }
-    async function handlejson(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://gissose2020chris.herokuapp.com";
-        url += "/json";
-        let query: URLSearchParams = new URLSearchParams(<any>formData);
-        url = url += "?" + query.toString();
-        let response: Response = await fetch(url);
-        let responseText: string = await response.text();
-        let responseJSON: JSON = JSON.parse(responseText);
-        console.log(responseJSON);
-        console.log(responseText);
-        server.innerHTML = responseText;
-
-         
-        }
+    // ansteuerung des 1 Buttons 
+    async function consolenausgabebutton(): Promise<void> {
+        consolenausgabe(await splitrequest(senden()));
+    }
+    // ansteuerung des 2 Buttons 
+    async function htmlausgabebutton(): Promise<void> {
+        htmlausgabe( await splitrequest(senden()));
     }
 
+    // ausgabe des Strngs als html 
+    function htmlausgabe(_arraysplit: string[]): void {
+
+        (<HTMLElement>document.getElementById("anzeige")).innerHTML  = _arraysplit[0];
+
+    }
+    // ausgebe der json in der Console
+    function consolenausgabe(_arraysplit: string[]): void {
+        let ausgabe: string = JSON.parse(_arraysplit[1]);
+        console.log(ausgabe);
+    }
+
+    // die response an der verhervestgelekten teil trennen 
+    async function splitrequest(_url: RequestInfo): Promise<string[]> {
+        let response: Response = await fetch(_url);
+        let response2: string = await response.text();
+        let arrayrequest: string[] = response2.split("&&");
+        return arrayrequest;
+    }
 
 
-    
 
+}   
